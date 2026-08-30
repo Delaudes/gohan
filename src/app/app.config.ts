b7b6d36685569b60b91a6f334bbccf113fd9cc4a
Sodel <delaude.so@gmail.com>
2026-08-30 15:23:57 +0200

    feat: add service worker support and PWA configuration with manifest and icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 import { deviceIdInterceptor } from '../infra/http/device-id.interceptor';
@@ -9,6 +10,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([deviceIdInterceptor]))
+    provideHttpClient(withInterceptors([deviceIdInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
